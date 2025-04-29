8006fd30ceaad99d95f42ea37df4ba2a5e7bd464
gvfabri <gabriel_fabri13@outlook.com>
2025-04-28 21:19:49 -0300

    iniciando gerador de quadrupla

--- geradordequadrupla.c
+++ geradordequadrupla.c
@@ -4,44 +4,75 @@
 #include"global.h"
 #include"util.h"
 
-char* generateCode(TreeNode *t) {
+
+void genStmt(TreeNode *tree)
+{
+	int loc;
+	switch(tree->kind.stmt)
+	{
+		case IfK:
+			break;
+		case WhileK:
+			break;
+		case returnK:
+			break;
+		case AtrK:
+			break;
+		case Vark:
+			break;
+		case FunK:
+			break;
+		case CallK:
+			break;
+		case ParamK:
+			break;
+		default:
+			break;
+
+	}
+}
+
+void genExp(TreeNode *tree)
+{
+	int loc;
+	switch(tree->kind.exp)
+	{
+		case OpK:
+			break;
+		case ConstK:
+			break;
+		case IdK:
+			break;
+		case TypeK:
+			break;
+		case VetK:
+			break;
+		default:
+			break;
+	}
+
+}
+void cGen(TreeNode *t)
+{
+	switch(t->nodeKind)
+	{
+		case StmtK:
+			genStmt(t);
+			break;
+		case ExpK:
+			genExp(t);
+			break;
+		default:
+			break;
+	}
+	cGen(t->sibling);
+}
+
+char *generateCode(TreeNode *t) {
     if (t == NULL) return NULL;
 
-    // Pós-ordem nos filhos
-    char *left = generateCode(t->child[0]);
-    char *right = generateCode(t->child[1]);
-    char *extra = generateCode(t->child[2]);
+    cGen(t);
 
-    char *result = NULL;
 
-    switch (t->kind) {
-        case ConstK: {
-            
-            break;
-        }
-        case IdK: {
-            break;
-        }
-        case OpK: {
-            
-            break;
-        }
-        case AtrK: {
-           
-            break;
-        }
-        case CallK: {
-            
-            break;
-        }
-        case IfK: {
-            
-            break;
-        }
-        default:
-            break;
-    }
 
-    generateCode(t->sibling); // percorre irmãos
-    return result;
 }
